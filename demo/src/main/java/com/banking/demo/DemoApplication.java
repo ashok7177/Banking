07477d38ed24f008b38f057242e0891d5196cbd4
a <a@DESKTOP-K4UFO9B>
2021-08-09 15:52:23 +0530

    modified in main method

--- demo/src/main/java/com/banking/demo/DemoApplication.java
+++ demo/src/main/java/com/banking/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("application is started");
+		System.out.println("application is started and pushing to git");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
